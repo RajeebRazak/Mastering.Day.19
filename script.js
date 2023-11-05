1a36560f249313e701d319ec60cacfca0cf07e94
RajeebRazak <145429667+RajeebRazak@users.noreply.github.com>
2023-11-05 22:44:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var tbody = document.querySelector("tbody");
+var pageU1 = document.querySelector(".page");
+var itemshow = document.querySelector("#itemperpage")
+var tr = tbody.querySelector("tr")
